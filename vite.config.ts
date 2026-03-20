0c6c8f7dd97dddebbea5354f61a7a9547e46a102
Gabriecarvalho <gabrielcarvalho8@hotmail.com.br>
2026-03-20 12:58:02 -0300

    chore(30):add tailwind depedencie

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
+// vite.config.ts
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true, // Isso silencia avisos de bibliotecas externas
+        silenceDeprecations: ['import'], // Silencia especificamente o erro do @import
+      },
+    },
+  },
+});
