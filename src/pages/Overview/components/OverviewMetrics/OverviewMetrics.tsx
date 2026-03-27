d4cc5a9cbec4ccc834f27430ae550a6250184afd
Elbert Jean <elbertjeandossantos@gmail.com>
2026-03-27 09:23:28 -0300

    fix(32): change to the card labels

--- src/pages/Overview/components/OverviewMetrics/OverviewMetrics.tsx
+++ src/pages/Overview/components/OverviewMetrics/OverviewMetrics.tsx
@@ -3,20 +3,6 @@ import './OverviewMetrics.scss';
 
 export default function OverviewMetrics() {
   const financialMetrics = [
-    // Soma da coluna valor_alocado da tabela compras_projeto para este projeto.
-    {
-      title: 'Valor Total Alocado',
-      value: 'R$ 50,00',
-      icon: <CircleDollarSign size={24} />,
-      bgClass: 'bg-green'
-    },
-    // Soma da coluna estimativa_horas da tabela tarefas_projeto para as tarefas relacionadas a este projeto.
-    {
-      title: 'Total de Horas Estimadas',
-      value: '92,0',
-      icon: <Timer size={24} />,
-      bgClass: 'bg-blue'
-    },
     // Soma da coluna horas_trabalhadas da tabela tempo_tarefas para as tarefas relacionadas a este projeto 
     // (através da tabela tarefas_projeto).
     {
@@ -25,10 +11,24 @@ export default function OverviewMetrics() {
       icon: <Clock size={24} />,
       bgClass: 'bg-orange'
     },
+    // Soma da coluna estimativa_horas da tabela tarefas_projeto para as tarefas relacionadas a este projeto.
+    {
+      title: 'Total de Horas Previstas',
+      value: '92,0',
+      icon: <Timer size={24} />,
+      bgClass: 'bg-blue'
+    },
+    // Soma da coluna valor_alocado da tabela compras_projeto para este projeto.
+    {
+      title: 'Valor Empenhado',
+      value: 'R$ 50,00',
+      icon: <CircleDollarSign size={24} />,
+      bgClass: 'bg-green'
+    },
     //  Soma do custo_estimado_usd da tabela materiais_engenharia multiplicado pela quantidade_empenhada 
     // da tabela empenho_materiais para os materiais relacionados a este projeto (através da tabela empenho_materiais).
     {
-      title: 'Custo Total Empenhado',
+      title: 'Gasto Total',
       value: 'R$ 7.053,04',
       icon: <Boxes size={24} />,
       bgClass: 'bg-purple'
