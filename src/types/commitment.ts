79b79c6e736fb05b8db74e6550427d3de04254a0
Cainã Melo <163151125+CainaNascimentoMelo@users.noreply.github.com>
2026-04-03 14:34:48 -0300

    feat(36): Add types commitment material

---
+++ src/types/commitment.ts
@@ -0,0 +1,9 @@
+export type Material = {
+  id: string;
+  nome: string;
+  categoria: string;
+  quantidade_empenhada: number;
+  custo_unitario: number;
+  data_empenho: string;
+  status: string;
+};
