73a32710e414b0c5da7aa533af0070d7e1a6fec9
gabrielvascf <93106982+gabrielvascf@users.noreply.github.com>
2026-04-03 20:50:59 -0300

    feat(34): add hours tracking component to overview screen

--- src/pages/Overview/OverviewScreen.tsx
+++ src/pages/Overview/OverviewScreen.tsx
@@ -1,11 +1,17 @@
 import OverviewMetrics from './components/OverviewMetrics/OverviewMetrics';
 import ProjectLayout from '../../components/ProjectLayout/ProjectLayout';
 import './OverviewScreen.scss';
+import HoursTracking from './components/HoursTracking/HoursTracking';
 
 export default function Overview() {
   return (
     <ProjectLayout pageClassName="overview-page" contentClassName="overview-content">
-      {(data) => <OverviewMetrics financeiro={data?.financeiro} />}
+      {(data) => (
+        <>
+          <OverviewMetrics financeiro={data?.financeiro} />
+          <HoursTracking />
+        </>
+      )}
     </ProjectLayout>
   );
 }
