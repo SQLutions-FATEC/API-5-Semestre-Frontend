4dbb559870f088ac18fbe7271279e157dece1925
d-broder <daniel_sendreti@hotmail.com>
2026-05-28 12:36:37 -0300

    chore(129): adjust rules for TypeScript and React hooks

--- eslint.config.js
+++ eslint.config.js
@@ -43,8 +43,14 @@ export default defineConfig([
       'react/react-in-jsx-scope': 'off',
 
       '@typescript-eslint/no-unused-vars': ['warn'],
-
-      'react-hooks/rules-of-hooks': 'error',
+      
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'react/no-unescaped-entities': 'off',
+      
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      
+      'react-hooks/rules-of-hooks': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
     },
   },
